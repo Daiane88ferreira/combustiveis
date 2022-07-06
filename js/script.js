7ca35827787132ffdf7ab435c9c70628dcff0fb9
p626582 <carlos.hnunes@castgroup.com.br>
2022-07-06 16:00:39 -0300

    combustiveis

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function combustivel() {
+    var gasolina =document.getElementById('gasolina').value;
+    var etanol =document.getElementById('etanol').value; 
+    var result =document.getElementById('result');
+  
+//trocar virgula por ponto
+    gasolina = gasolina.replace(',', '.');
+    etanol = etanol.replace(',','.');
+
+    if (gasolina != '' && etanol != '') {
+        // calcular o melhor combustível
+
+    var maisBarato =etanol/gasolina;
+
+
+    if (maisBarato >= 0.7) {
+        result.innerHTML = 'Melhor abastecer com gasolina';
+        
+    }
+
+    else {
+        result.innerHTML = 'Melhor abastecer com etanol';
+    }
+}
+
+    else {
+        alert('Preencha todos os campos!');
+    }
+
+}
+
+function verificarInput(event) {
+    var letra = event.key;
+
+    if (letra != 0 && letra != 1 && letra != 2 && letra != 3 && letra != 4 && letra != 5 && letra != 6 && letra != 7 && letra != 8 && letra != 9 && letra != ',' && letra != '.' && letra != 'Tab' && letra != 'Enter' && letra != 'Backspace' && letra != 'Delete' && letra != 'ArrowLeft' && letra != 'ArrowRight')   {
+        alert('Caractere inválido!');
+        document.getElementById('gasolina').value = '';
+        document.getElementById('etanol').value = '';
+
+    }
+}
